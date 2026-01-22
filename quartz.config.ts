2d79dbc5e5c337da3bd7a4fa730109c12e87ead6
su-peace <144257116+su-peace@users.noreply.github.com>
2026-01-22 20:19:22 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: T-Blog-T",
+    pageTitle: "T-Blog-T",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
